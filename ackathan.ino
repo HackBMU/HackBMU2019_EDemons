1da4c651e12767067e74db754c5f8099108e0485
atomract <46929915+atomract@users.noreply.github.com>
2019-03-26 22:09:51 +0530

    Add files via upload

---
+++ ackathan.ino
@@ -0,0 +1,39 @@
+ int t=1;
+ const int t1 = 13;
+const int s = 12;
+ long sd,td;
+ 
+ 
+  void setup()
+{
+Serial.begin(9600);
+pinMode(s,INPUT);
+pinMode(t1,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(t1, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(t1, HIGH);
+  delayMicroseconds(10);
+
+  
+  digitalWrite(t1, LOW);
+
+  sd=pulseIn(s, HIGH);
+
+  td=sd/58.2;
+  Serial.println(td);
+  delay(50);
+
+Serial.println("See It's working or not: ");
+ Serial.println(t);
+ t++;
+ if(t==2)
+ {
+  Serial.print("et's see");
+  }
+ delay(1000);
+}
